d6101fb5cafdb073b9e4550af816137462f4c4fe
khanoyanv <113764269+khanoyanv@users.noreply.github.com>
2022-10-02 20:48:15 +0400

    Update BeerListItem.js

--- src/components/BeerListItem.js
+++ src/components/BeerListItem.js
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom'
 function BeerListItem({beer, goToSelectedBeer}) { 
 
   return (
-      <Link to={beer.path}>
-        <div className='beer-list-item' id={beer.id} onClick={e => goToSelectedBeer(e, beer)}>
+        <Link to={beer.path} className='beer-list-item' id={beer.id} onClick={e => goToSelectedBeer(e, beer)}>
             <img src={beer.image_url} className='beer-list-item-image' id={beer.id} />
             <h2 className='beer-list-item-name' id={beer.id} > {beer.name} </h2>
             <h4 className='beer-list-item-price' id={beer.id} > {beer.price}$ </h4>
-        </div> 
-      </Link>
+        </Link> 
   )
 }
 
